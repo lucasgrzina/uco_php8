bff1866ea193ad6b7509491608ed8a4ae9c97d1a
Lucas <lgrzina@identidad-digital.com>
2023-02-24 11:58:47 -0300

    a

---
+++ resources/lang/es/emails.php
@@ -0,0 +1,18 @@
+<?php
+return [
+    'general' => [
+        'desuscribirme' => 'Quiero desuscribirme',
+        'perfil' => 'Actualizar perfil',
+        'tyc' => 'Términos y condiciones'
+    ],
+    'registro' => [
+        'texto' => 'A partir de ahora, para acceder a tu perfil, podrás hacerlo ingresando tu nombre de usuario <span style="color: #8E8063;">@USUARIO</span> y tu contraseña.',
+        'btn_texto' => 'INGRESÁ AL SITIO',
+    ],
+    'recuperar' => [
+        'linea1' => 'Hemos recibido una solicitud para <strong>restablecer la contraseña</strong> de tu cuenta. Si es así, ingresá de nuevo utilizando esta por única vez; ',
+        'linea2' => 'Al hacerlo no olvides cambiarla por una <strong>nueva y personal</strong>.',
+        'linea3' => 'Si no solicitaste el restablecimiento de la contraseña, ignorá este correo electrónico.',
+        'btn_texto' => 'RESTABLECER CONTRASE&Ntilde;A',
+    ]
+];
