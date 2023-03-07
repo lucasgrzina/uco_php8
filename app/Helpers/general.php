4b0b4b221f7eba6b6ea0c0e5fdb0b74759210554
Lucas <lgrzina@identidad-digital.com>
2023-03-07 09:39:05 -0300

    a

--- app/Helpers/general.php
+++ app/Helpers/general.php
@@ -23,48 +23,17 @@ function randomPassword() {
 }
 
 function randomPassword2($password_length=6) {
-    $uppercase_letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    $numbers = '0123456789';
+    $longitud = $password_length; // Cambia el valor aquí para ajustar la longitud de la cadena
+    $cadena = '';
 
-    // Genera una contraseña aleatoria que contenga al menos una letra mayúscula y al menos un número
-    do {
-      $password = '';
-      $has_uppercase_letter = false;
-      $has_number = false;
+    while (true) {
+        $cadena = bin2hex(random_bytes($longitud)); // Genera una cadena aleatoria en hexadecimal
 
-      // Genera una contraseña aleatoria de longitud $password_length
-      for ($i = 0; $i < $password_length; $i++) {
-        $characters = '';
-
-        // Agrega letras mayúsculas si aún no se ha agregado ninguna
-        if (!$has_uppercase_letter || !$has_number) {
-          if (!$has_uppercase_letter) {
-            $characters .= $uppercase_letters;
-          }
-
-          if (!$has_number) {
-            $characters .= $numbers;
-          }
-        } else {
-          // Si ya se ha agregado al menos una letra mayúscula y un número, se pueden agregar letras minúsculas y caracteres especiales
-          $characters = $uppercase_letters . strtolower($uppercase_letters) . $numbers;
+        // Comprueba si la cadena contiene al menos una letra minúscula, una letra mayúscula y un número
+        if (preg_match('/^(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[0-9]).{'.$longitud.',}$/',$cadena)) {
+            return $cadena;
         }
-
-        // Agrega un caracter aleatorio del conjunto de caracteres disponibles
-        $password .= $characters[rand(0, strlen($characters) - 1)];
-
-        // Verifica si se ha agregado una letra mayúscula y un número a la contraseña
-        if (preg_match('/[A-Z]/', $password)) {
-          $has_uppercase_letter = true;
-        }
-
-        if (preg_match('/[0-9]/', $password)) {
-          $has_number = true;
-        }
-      }
-    } while (!$has_uppercase_letter || !$has_number);
-
-    return $password;
+    }
 }
 
 function strEmpiezaCon($str,$comp) {
