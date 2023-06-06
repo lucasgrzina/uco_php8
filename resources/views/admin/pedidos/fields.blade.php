08b327fe132110461830d46c09d9d4f83f566dae
Lucas <lgrzina@identidad-digital.com>
2023-06-06 10:28:56 -0300

    a

--- resources/views/admin/pedidos/fields.blade.php
+++ resources/views/admin/pedidos/fields.blade.php
@@ -7,7 +7,7 @@
 <!-- Email Field -->
 <div class="form-group col-sm-6" :class="{'has-error': errors.has('email')}">
     {!! Form::label('email', 'Email') !!}
-    {!! Form::text('email', null, ['class' => 'form-control','v-model' => 'selectedItem.email']) !!}
+    <span class="form-control">(% selectedItem.email %)</span>
     <span class="help-block" v-show="errors.has('email')">(% errors.first('email') %)</span>
 </div>
 
